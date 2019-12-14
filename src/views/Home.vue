a76f301a0eaba89688bb21ea378e1c655a318454
yann <yann.boisselier@gmail.com>
2019-12-14 17:51:03 +0100

    style du header comme la page du site vitrine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <header
-      style="background:#74CFE2;padding:50px 0;color: #044389"
-      class="has-text-centered"
-    >
+    <header class="has-text-centered">
       <div class="titles">
         <h1 class="title is-1">Test du syndrome de l'imposteur</h1>
-        <h2 class="subtitle">Échelle de Clance</h2>
+        <h2 class="subtitle is-3">Échelle de Clance</h2>
       </div>
     </header>
     <div style="max-width: 700px" class="container">
@@ -30,16 +27,43 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: abril fatface;
+  src: url("https://fonts.gstatic.com/s/abrilfatface/v11/zOL64pLDlL1D99S8g8PtiKchq-lmiA.ttf");
+}
+@font-face {
+  font-family: josefin slab;
+  src: url("https://fonts.gstatic.com/s/josefinslab/v10/lW-mwjwOK3Ps5GSJlNNkMalvESu6KerlFAke7w.woff2");
+}
 header {
-  background: #74cfe2;
-  padding: 50px 0;
+  background-color: #74cfe2;
+  padding: 100px 0;
+  background-image: url("https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/1984359/761034_256831.png");
+  background-repeat: no-repeat;
+  background-position: top left;
+  background-size: 400px;
+  background-position: left 30px;
+}
+
+@media screen and (max-width: 769px) {
+  header {
+    background-position: center 30px;
+    background-size: 140px;
+    padding-top: 170px;
+  }
 }
 
 header .title {
+  max-width: 800px;
+  margin: auto;
+  font-family: "abril fatface", georgia, serif;
   color: #044389;
+  font-weight: 400;
 }
 
 header .subtitle {
-  color: ##044389;
+  font-family: "josefin slab", georgia, serif;
+  padding-top: 50px;
+  color: white;
 }
 </style>
