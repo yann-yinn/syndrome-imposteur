18389119b1aa388c66d5b8fbc76d4a5f97d9f655
yann <yann.boisselier@gmail.com>
2019-12-14 18:04:30 +0100

    css

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://syndromeimposteur.fr">
+        <img
+          src="https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/1984359/136944_645583.png"
+        />
+        <strong style="margin-left:20px;">Aurélie da Silva · Coach</strong>
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-info" href="https://www.syndromeimposteur.fr/">
+              <strong>Retour au site</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <NavBar />
     <header class="has-text-centered">
       <div class="titles">
         <h1 class="title is-1">Test du syndrome de l'imposteur</h1>
@@ -17,53 +18,13 @@
 <script>
 // @ is an alias to /src
 import ImposteurForm from "@/components/ImposteurForm.vue";
+import NavBar from "@/components/NavBar.vue";
 
 export default {
   name: "home",
   components: {
-    ImposteurForm
+    ImposteurForm,
+    NavBar
   }
 };
 </script>
-
-<style scoped>
-@font-face {
-  font-family: abril fatface;
-  src: url("https://fonts.gstatic.com/s/abrilfatface/v11/zOL64pLDlL1D99S8g8PtiKchq-lmiA.ttf");
-}
-@font-face {
-  font-family: josefin slab;
-  src: url("https://fonts.gstatic.com/s/josefinslab/v10/lW-mwjwOK3Ps5GSJlNNkMalvESu6KerlFAke7w.woff2");
-}
-header {
-  background-color: #74cfe2;
-  padding: 100px 0;
-  background-image: url("https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/1984359/761034_256831.png");
-  background-repeat: no-repeat;
-  background-position: top left;
-  background-size: 400px;
-  background-position: left 30px;
-}
-
-@media screen and (max-width: 769px) {
-  header {
-    background-position: center 30px;
-    background-size: 140px;
-    padding-top: 170px;
-  }
-}
-
-header .title {
-  max-width: 800px;
-  margin: auto;
-  font-family: "abril fatface", georgia, serif;
-  color: #044389;
-  font-weight: 400;
-}
-
-header .subtitle {
-  font-family: "josefin slab", georgia, serif;
-  padding-top: 50px;
-  color: white;
-}
-</style>
